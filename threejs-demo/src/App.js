03419e64f8420e8db0c595ddc9e8a7106bfe3e22
谭金涛1 <15751666937@163.com>
2022-10-17 23:34:25 +0800

    测试

--- threejs-demo/src/App.js
+++ threejs-demo/src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <>
             <BrowserRouter basename={process.env.PUBLIC_URL}>
-                111
+                测试
                 <Routes>
                     <Route path="/demo"  exact={false} element={<Demo1Component />} />
                     <Route path="/three"  exact={false} element={<ThreeComponent />} />

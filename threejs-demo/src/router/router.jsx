9bba1eb6f256d45f4dfe258ac30510bc321283a1
tjt740 <15751666937@163.com>
2022-10-25 00:11:22 +0800

    'init'

---
+++ threejs-demo/src/router/router.jsx
@@ -0,0 +1,15 @@
+import PackagePreview3D from '../装箱demo';
+import Demo1Component from '../lessons/day1/index.tsx';
+import { Route, Routes } from 'react-router-dom';
+export function RouterCom() {
+    return (
+        <Routes>
+            <Route path="/demo" exact={false} element={<Demo1Component />} />
+            <Route
+                path="/package-preview"
+                exact={false}
+                element={<PackagePreview3D />}
+            />
+        </Routes>
+    );
+}

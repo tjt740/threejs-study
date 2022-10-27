56139a7b72a03d860b951416f71ec66f187abd47
tjt01070575 <tjt01070575@gongdao.com>
2022-10-27 17:56:54 +0800

    测试

--- threejs-demo/src/App.js
+++ threejs-demo/src/App.js
@@ -1,12 +1,43 @@
+import React, { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import './index.css';
+
 import { BrowserRouter } from 'react-router-dom';
-import { RouterCom } from './router/router.jsx';
+import { RouterCom, routePaths } from './router/router.jsx'; // 1.创建 RouterCom 组件
+
+const { Content, Sider } = Layout;
+const items = routePaths.map((item, index) => ({
+    key: String(index + 1),
+    label: item.name,
+    icon: item.icon,
+    children: item.children,
+}));
+
 function App() {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <RouterCom/>
-            </BrowserRouter>
-        </>
+        <Layout>
+            <Sider
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(value) => setCollapsed(value)}
+            >
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={items}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Content>
+                    11111
+                    <BrowserRouter>
+                        <RouterCom />
+                    </BrowserRouter>
+                </Content>
+            </Layout>
+        </Layout>
     );
 }
 

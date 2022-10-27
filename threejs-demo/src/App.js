675b61a1ce74656db8fe9aed4c2bbea81abc07d0
tjt740 <15751666937@163.com>
2022-10-27 22:44:58 +0800

    Merge branch 'master' into daily/2022.10.24
    # Conflicts:
    #	threejs-demo/package-lock.json
    #	threejs-demo/src/App.js
    #	threejs-demo/src/index.css
    #	threejs-demo/src/pages/lessons/day1/index.jsx
    #	threejs-demo/src/router/router.jsx

--- threejs-demo/src/App.js
+++ threejs-demo/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
+import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
-
-// 设置路由 → 引入BrowserRouter →
-import { BrowserRouter } from 'react-router-dom';
-import { RouterCom } from './router/router.jsx';
-import 'antd/dist/antd.css';
 import './index.css';
-const { Header, Content, Footer, Sider } = Layout;
+
+import { BrowserRouter } from 'react-router-dom';
+import { RouterCom, routePaths } from './router/router.jsx'; // 1.创建 RouterCom 组件
+
+const { Content, Sider } = Layout;
+const items = routePaths.map((item, index) => ({
+    key: String(index + 1),
+    label: item.name,
+    icon: item.icon,
+    children: item.children,
+}));
 
 function App() {
     const [collapsed, setCollapsed] = useState(false);

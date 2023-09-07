dd0fdaa7b5f89bf4760590a65b5090ed897e5757
t <15751666937@163.com>
2023-09-07 23:52:50 +0800

    1234

--- threejs-demo/src/pages/3D看房2/class/room.js
+++ threejs-demo/src/pages/3D看房2/class/room.js
@@ -2,64 +2,31 @@ import * as THREE from 'three';
 
 export class CreateRoomList {
     constructor(areasList, roomName, scene) {
-        console.log(roomName);
-        this.roomShape = new THREE.Shape();
+        this.areasList = areasList;
+        this.roomName = roomName;
+        this.scene = scene;
 
-        for (let i = 0; i < areasList.length; i++) {
+        this.roomShape = new THREE.Shape();
+        for (let i = 0; i < this.areasList.length; i++) {
             if (!i) {
                 this.roomShape.moveTo(
-                    areasList[i].x / 100,
-                    areasList[i].y / 100
+                    this.areasList[i].x / 100,
+                    this.areasList[i].y / 100
                 );
             } else {
                 this.roomShape.lineTo(
-                    areasList[i].x / 100,
-                    areasList[i].y / 100
+                    this.areasList[i].x / 100,
+                    this.areasList[i].y / 100
                 );
             }
+
+            console.log(this.areasList[i]);
         }
-
-        // const canvas = document.createElement('canvas');
-        // canvas.width = 1024;
-        // canvas.height = 1024;
-        // const ctx = canvas.getContext('2d');
-        // ctx.font = '12px Arial';
-        // ctx.textAlign = 'center';
-        // ctx.textBaseline = 'middle';
-        // ctx.font = 'bold 200px Arial';
-        // ctx.fillStyle = '#ffffff'; //  设置文本字体和大小
-        // ctx.fillText(roomName, 512, 512);
-        // const canvasTexture = new THREE.CanvasTexture(canvas);
-        // canvasTexture.minFilter = THREE.LinearFilter;
-        // canvasTexture.magFilter = THREE.LinearFilter;
-
-        this.canvas = document.createElement('canvas');
-        this.canvas.width = 1024;
-        this.canvas.height = 1024;
-        this.ctx = this.canvas.getContext('2d');
-        this.ctx.font = '12px Arial';
-        this.ctx.textAlign = 'center';
-        this.ctx.textBaseline = 'middle';
-        this.ctx.font = 'bold 200px Arial';
-        this.ctx.fillStyle = '#ffffff'; //  设置文本字体和大小
-        this.ctx.fillText(roomName, 512, 512);
-        this.canvasTexture = new THREE.CanvasTexture(this.canvas);
-        this.canvasTexture.minFilter = THREE.LinearFilter;
-        this.canvasTexture.magFilter = THREE.LinearFilter;
-
-        const textSprite = new THREE.Sprite(
-            new THREE.SpriteMaterial({
-                map: this.canvasTexture,
-                depthTest: false, // 不进行深度检测
-            })
-        );
-        // scene.add(textSprite)
 
         this.shapeGeometry = new THREE.ShapeGeometry(this.roomShape);
         this.shapeMaterial = new THREE.MeshBasicMaterial({
             side: THREE.DoubleSide,
             color: new THREE.Color(Math.random(), Math.random(), Math.random()),
-            // map: textSprite,
             transparent: true,
         });
 
@@ -70,35 +37,39 @@ export class CreateRoomList {
         this.generateMesh();
 
         // 创建canvas文案
-        // this.creatCanvas(roomName);
+        this.creatCanvas();
     }
     // 创建模型
     generateMesh() {
-        return new THREE.Mesh(this.shapeGeometry, this.shapeMaterial);
+        this.scene.add(new THREE.Mesh(this.shapeGeometry, this.shapeMaterial));
     }
     // 创建canvas文案
-    creatCanvas(roomName) {
+    creatCanvas() {
         this.canvas = document.createElement('canvas');
         this.canvas.width = 1024;
         this.canvas.height = 1024;
         this.ctx = this.canvas.getContext('2d');
-        this.ctx.font = '12px Arial';
+        this.ctx.font = '250px Arial';
         this.ctx.textAlign = 'center';
         this.ctx.textBaseline = 'middle';
-        this.ctx.font = 'bold 200px Arial';
-        this.ctx.fillStyle = '#ffffff'; //  设置文本字体和大小
-        this.ctx.fillText(roomName, 512, 512);
+        this.ctx.fillStyle = 'red'; //  设置文本字体和大小
+        this.ctx.fillText(this.roomName, 512, 256);
         this.canvasTexture = new THREE.CanvasTexture(this.canvas);
-        this.canvasTexture.minFilter = THREE.LinearFilter;
-        this.canvasTexture.magFilter = THREE.LinearFilter;
 
-        const textSprite = new THREE.Sprite(
+        this.textSprite = new THREE.Sprite(
             new THREE.SpriteMaterial({
                 map: this.canvasTexture,
                 depthTest: false, // 不进行深度检测
             })
         );
 
-        return textSprite;
+        // 设置精灵文案位置
+        this.textSprite.position.set(
+            this.areasList[0].x / 100,
+            1,
+            this.areasList[0].y / 100
+        );
+
+        this.scene.add(this.textSprite);
     }
 }
--- threejs-demo/src/pages/3D看房2/index.jsx
+++ threejs-demo/src/pages/3D看房2/index.jsx
@@ -110,6 +110,10 @@ export default function ThreeComponent() {
             scene.position.z
         );
 
+        //  创建XYZ直角坐标系  (红色代表 X 轴. 绿色代表 Y 轴. 蓝色代表 Z 轴.)，帮助我们查看3维坐标轴
+        const axesHelper = new THREE.AxesHelper(25);
+        //  坐标辅助线添加到场景中
+        scene.add(axesHelper);
         /*
          * ------------ start ----------
          */
@@ -165,7 +169,6 @@ export default function ThreeComponent() {
                     wallRelation,
                 } = data;
 
-                // console.log(roomList);
                 // 创建平面图
                 roomList.forEach((item, index) => {
                     const { areas, roomName } = item;
@@ -179,7 +182,6 @@ export default function ThreeComponent() {
                         roomName,
                         scene
                     );
-                    scene.add(roomAreas.generateMesh());
 
                     // 场景添加class返回值
                 });

ea5e8e866ce8caba9b57f1502a539e6a6fa4e373
t <15751666937@163.com>
2023-10-04 22:25:25 +0800

    12345

--- threejs-demo/src/pages/cesium/cesium基础使用/cesium/cesium.js
+++
@@ -1,4 +0,0 @@
-import * as THREE from 'three';
-
-// 引入cesium.js
-import * as Cesium from 'cesium';
--- threejs-demo/src/pages/cesium/cesium基础使用/index.jsx
+++ threejs-demo/src/pages/cesium/cesium基础使用/index.jsx
@@ -1,29 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 
-import scene from './three/scene';
-import camera from './three/camera';
-import renderer from './three/renderer';
-import controls from './three/controls';
-import animation from './three/animation';
-import gui from './three/gui';
-import gsap from './three/gsap';
-import windowDom2 from './three/windowDom2';
-// 添加光线
-import ambientLight from './three/ambientLight';
-import directionalLight from './three/directionalLight';
-// 辅助线
-import axesHelper from './three/axesHelper';
+// 引入cesium.js
+// import * as Cesium from 'cesium';
+// 导入widgets.css 【必须】
+// import 'cesium/Build/Cesium/Widgets/widgets.css';
 
-// cesium
-import cesium from './cesium/cesium';
-
-export default function ThreeComponent() {
+export default function CesiumComponent() {
     const containerRef = useRef(null);
 
-    const init = () => {
-        // DOM承载渲染器
-        containerRef.current.appendChild(renderer.domElement);
-    };
+    const init = () => {};
 
     useEffect(() => {
         init();

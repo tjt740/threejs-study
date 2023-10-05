2b93cb8d4f8814ff3028759ec3b78c81fd1ab509
t <15751666937@163.com>
2023-10-05 16:50:32 +0800

    cesium初始化加载

--- threejs-demo/src/pages/cesium/cesium基础使用/index.jsx
+++ threejs-demo/src/pages/cesium/cesium基础使用/index.jsx
@@ -1,14 +1,37 @@
 import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
 // 引入cesium.js
-// import * as Cesium from 'cesium';
-// 导入widgets.css 【必须】
+import * as Cesium from 'cesium';
+import { Viewer } from 'resium';
+// 引入widgets.css
 // import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 export default function CesiumComponent() {
-    const containerRef = useRef(null);
+    const cesiumRef = useRef(null);
 
-    const init = () => {};
+    const init = () => {
+        const viewer = new Cesium.Viewer(
+            cesiumRef.current || 'cesium-container',
+            {
+                infoBox: false,
+            }
+        );
+        // viewer.scene.globe.depthTestAgainstTerrain = true;
+        // viewer.scene.globe.enableLighting = true;
+        // viewer.scene.globe.baseColor = new THREE.Color(0.3, 0.3, 0.3);
+        // viewer.scene.globe.showGroundAtmosphere = true;
+        // viewer.scene.globe.show = true;
+        // viewer.scene.globe.enableLighting = true;
+        // viewer.scene.globe.showWaterEffect = true;
+        // viewer.scene.globe.showGroundAtmosphere = true;
+        // viewer.scene.globe.show = true;
+        // viewer.scene.globe.enableLighting = true;
+        // viewer.scene.globe.showWaterEffect = true;
+        // viewer.scene.globe.showGroundAtmosphere = true;
+        // viewer.scene.globe.show = true;
+        // ;
+    };
 
     useEffect(() => {
         init();
@@ -16,7 +39,15 @@ export default function CesiumComponent() {
 
     return (
         <>
-            <div id="container" ref={containerRef}></div>
+            {/* 一定要限制高度 */}
+            <div
+                id="cesium-container"
+                ref={cesiumRef}
+                style={{
+                    height: '100vh',
+                    width: '100%',
+                }}
+            ></div>
         </>
     );
 }
